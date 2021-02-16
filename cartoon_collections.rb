6118137358a82d31f5ba11b0aedfac7d282a8c1b
Jouse then <jthen93@gmail.com>
2021-02-16 06:39:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,12 +26,12 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  binding.pry
   ingredients.each do |ingredient|
     if cheese_types.include(ingredient)
      cheese_found = ingredient
      break
     else 
+      binding.pry
      cheese_found = nil
     end
   end
