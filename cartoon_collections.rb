c8fc966a9bb8bc9c502c34f0d68375533ea061c3
Jouse then <jthen93@gmail.com>
2021-02-16 06:23:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,6 @@ def long_planeteer_calls(calls)
   return anyfour
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
