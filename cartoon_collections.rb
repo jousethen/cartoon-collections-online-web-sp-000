b4f28f6ed68f194d3e79dd3888c3e88a8edb8a2a
Jouse then <jthen93@gmail.com>
2021-02-16 06:44:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,7 @@ def find_the_cheese(ingredients)
   ingredients.each do |ingredient|
     if cheese_types.include?(ingredient)
      cheese_found = ingredient
+     binding.pry
      break
     else 
      cheese_found = nil
