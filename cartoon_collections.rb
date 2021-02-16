5353a6f6c30ddb1c5ed73aebcf7d60a33d904882
Jouse then <jthen93@gmail.com>
2021-02-16 06:07:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elems)
+  elems.collect do |elem|
+    elem.
+  end
 end
 
 def long_planeteer_calls# code an argument here
